c345e50f7668507d83802255a33bde81bf3bdb65
Dwigth Astacio <dwigth32@outlook.es>
2017-12-29 12:09:34 -0600

    CFM v0.0.9 -modulo prospectos
    -Edición de prospectos
    -Busqueda de prospectos por nombre (busqueda principal)
    -Busqueda avanzada de prospectos
    -Eliminación de prospectos

--- src/pages/propects/prospect.service.ts
+++ src/pages/propects/prospect.service.ts
@@ -1,110 +1,121 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireAuth }     from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { AngularFireAction} from "angularfire2/database";
-import { Observable } from 'rxjs/observable';
+import { AngularFireAction } from "angularfire2/database";
+import { Observable } from 'rxjs/Observable';
 import { Prospect } from "./prospect";
 import { Subject } from "rxjs/Subject";
-import { Http, RequestOptions } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import * as moment from 'moment';
 @Injectable()
 
 export class ProspectService {
 
-  items:Observable<any[]>;
+  items: Observable<any[]>;
   items$: Observable<AngularFireAction<firebase.database.DataSnapshot>[]>
-  name$:BehaviorSubject<string|null>;
+  name$: BehaviorSubject<string | null>;
   prospects = [];
-  users:Observable<any[]>;
+  users: Observable<any[]>;
+  currentDay:number;
 
-  constructor(public db:AngularFireDatabase,public http:Http,public afAuth:AngularFireAuth ) {
-
+  constructor(public db: AngularFireDatabase, public http: Http, public afAuth: AngularFireAuth) {
+    let day = moment();
+    this.currentDay = Number(day.format('DDD'))
     moment.locale('es');
+  }
 
-
-}
-
-  refLis(){
+  refLis() {
     return this.db.list('prospects');
   }
-  refObj(itemkey){
-    return this.db.object('prospects/'+itemkey);
+  refObj(itemkey) {
+    return this.db.object('prospects/' + itemkey);
   }
 
-  getCoordis(){
+  getCoordis() {
     return this.users = this.db.list('users', value => value.orderByChild('accessLevel').equalTo("coordi")).valueChanges();
   }
 
-  createProspect(prospect:Prospect){
+  createProspect(prospect: Prospect) {
     let date = moment();
     this.refLis().push({
-    nombre:prospect.nombre,
-    apellidoPaterno:prospect.apellidoPaterno,
-    apellioMaterno:prospect.apellioMaterno,
-    edad:prospect.edad,
-    estado:prospect.estado,
-    telefono:prospect.telefono,
-    estatus:prospect.estatus,
-    atendio:prospect.atendio,
-    curso:prospect.curso,
-    fecha: date.format("MMMM D YYYY").toString(),
-    comentario:prospect.comentario,
-    fuente:prospect.fuente,
-    precio:prospect.precio,
-    //checkbox:prospect.checkbox
-  }).then(val => {
-    this.refObj(val.key).update({
-      key:val.key
+      name: prospect.name,
+      lastname: prospect.lastname,
+      lastname2: prospect.lastname2,
+      age: prospect.age,
+      state: prospect.state,
+      phone: prospect.phone,
+      status: prospect.status,
+      attended: prospect.attended,
+      course: prospect.course,
+      date: date.format("MMMM D YYYY").toString(),
+      coment: prospect.coment,
+      source: prospect.source,
+      price: prospect.price,
+      day: Number(date.format("DDD"))
+      //checkbox:prospect.checkbox
+    }).then(val => {
+      this.refObj(val.key).update({
+        key: val.key
+      });
     });
-  });
   }
 
-getProspect(){
-return this.db.list("prospects",val => val.orderByChild("nombre").limitToFirst(20)).valueChanges();
-}
-  searchProspectByProperty(n,property){
-    let result:Observable<any[]>;
-    return result = this.db.list("prospects",val => val.orderByChild(property).equalTo(n)).valueChanges();
+  getProspect() {
+    //  this.db.list('prospects')
   }
 
-searchByName(name:string){
-console.log(name)
-return this.db.list("prospects", val =>
- val.orderByChild("nombre").equalTo(name)
-). valueChanges();
-}
-
-editProspect(){
-
-}
-
-deleteProspect(item:Prospect){
-
-}
-
-getItems(ev:any){
-  //this.list = this.prosSrv.getProspect();
-  this.name$ = new BehaviorSubject(null);
-  let item = ev.target.value;
-  if (item && item.trim() != '') {
-    this.name$.next(item);
-    this.items$ = this.name$.switchMap(name =>
-    this.db.list('prospects', ref =>
-  name ? ref.orderByChild('nombre'). equalTo(name) : ref
-).snapshotChanges()
-);
-    //return this.list = this.prosSrv.searchByName(item);
+  searchProspectByProperty(n, property) {
+    let result: Observable<any[]>;
+    return result = this.db.list("prospects", val => val.orderByChild(property).equalTo(n).limitToFirst(50) ).valueChanges();
   }
-}
-getProspectbyHttp(){
 
-return this.http.get("https://pcfm-5eeb9.firebaseio.com/users")
-.map(res => res.json());
-}
+  searchByName(name: string) {
+    console.log(name)
+    return this.db.list("prospects", val =>
+      val.orderByChild("name").equalTo(name)
+    ).valueChanges();
+  }
 
-get(){
-  return this.prospects;
-}
+  editProspect(item) {
+    this.refObj(item.key).update({
+      name: item.name,
+      lastname: item.lastname,
+      lastname2: item.lastname2,
+      age: item.age,
+      state: item.state,
+      phone: item.phone,
+      status: item.status,
+      attended: item.attended,
+      course: item.course,
+      coment: item.coment,
+      source: item.source,
+      price: item.price
+    });
+  }
+
+  deleteProspect(item) {
+    this.refObj(item.key).remove();
+  }
+
+  getItems(ev: any) {
+    //this.list = this.prosSrv.getProspect();
+    this.name$ = new BehaviorSubject(null);
+    let item = ev.target.value;
+    if (item && item.trim() != '') {
+      this.name$.next(item);
+      this.items$ = this.name$.switchMap(name =>
+        this.db.list('prospects', ref =>
+          name ? ref.orderByChild('nombre').equalTo(name) : ref
+        ).snapshotChanges()
+      );
+      //return this.list = this.prosSrv.searchByName(item);
+    }
+  }
+
+  get() {
+    console.log(this.currentDay)
+    return this.db.list('prospects', val => val.orderByChild('day').equalTo(this.currentDay)). valueChanges();
+  }
 }
